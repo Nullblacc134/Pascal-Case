a4780f6361d77cb953c6215fa3ae3235100cf4d1
Clown-Box <Sm3n26@gmail.com>
2024-09-06 13:31:29 -0400

    Initial Commit

---
+++ Pascal Case/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pascal Case
+//
+//  Created by 6f on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pascal Case/Pascal_CaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pascal_CaseApp.swift
+//  Pascal Case
+//
+//  Created by 6f on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Pascal_CaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
